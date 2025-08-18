9b2664bb71c354bd7e07a31cdc39d1e695d5bf5d
Hemanth <hem123.gk@gmail.com>
2025-08-18 13:38:46 +0530

    Added repositories and boiler plate for code

---
+++ src/repositories/crud-repository.js
@@ -0,0 +1,54 @@
+const { StatusCodes } = require("http-status-codes");
+const { AppError } = require("../utils");
+
+class CrudRepository {
+  constructor(model) {
+    this.model = model;
+  }
+
+  async create(data) {
+    const response = await this.model.create(data);
+    return response;
+  }
+
+  async delete(data) {
+    const response = await this.model.destroy({
+      where: {
+        id: data,
+      },
+    });
+    if (!response) {
+      throw new AppError(
+        "Resource you want to delete is not found",
+        StatusCodes.NOT_FOUND
+      );
+    }
+    return response;
+  }
+
+  async getAll() {
+    const response = await this.model.findAll();
+    return response;
+  }
+
+  async get(id) {
+    const response = await this.model.findByPk(id);
+    if (!response) {
+      throw new AppError("Not Able to Find Resource", StatusCodes.NOT_FOUND);
+    }
+    return response;
+  }
+
+  async update(data, id) {
+    const response = await this.model.update(data, {
+      where: { id: id },
+    });
+
+    if (response[0] === 0) {
+      throw new AppError("Not Able to Find Resource", StatusCodes.NOT_FOUND);
+    }
+    return response;
+  }
+}
+
+module.exports = CrudRepository;
